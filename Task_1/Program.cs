85e32f7f99e7ef7c3b4c196fdcca42b12f62d2c3
«Kamila» <milo4-ka01@yandex.ru>
2022-03-25 17:27:25 +0300

    Задание 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задать массив, заполнить случайными положительными 3х значными числами
+// Показать количество нечетных\четных чисел
+
+int CountNechet( int[] arr )
+{
+    int count = 0;
+    int l = arr.Length;
+    for (int i = 0; i < l; i++)
+    {
+        if ( arr[i] % 2 != 0 )
+        {
+            count++ ; 
+        }
+    }
+    return count;
+}
+
+int[] array = new int[10];
+// Заполнение массива
+for (int i = 0; i < 10; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+}
+// Вывод массива
+for (int i = 0; i < 10; i++)
+{
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+
+int k = CountNechet( array );
+Console.WriteLine("Количество нечетных чисел: " + k);
